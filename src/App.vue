aa89349ebeaf59c047deb4831c16ec2de24d9dc0
Miguel Maestre <maestre.95@gmail.com>
2021-11-27 08:19:48 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Service pad</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+* {
+  vertical-align: baseline;
+  border: 0 none;
+  outline: 0;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-size: 16px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
